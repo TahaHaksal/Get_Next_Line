9316ef2fdc21bbc2fdb4d2875384acdf041df81f
Mustafa taha Haksal <mhaksal@k2m13s01.42kocaeli.com.tr>
2022-04-11 11:44:55 +0300

    new file:   Get_Next_Line/.DS_Store 	new file:   Get_Next_Line/get_next_line.c 	new file:   Get_Next_Line/get_next_line.h 	new file:   Get_Next_Line/get_next_line_bonus.c 	new file:   Get_Next_Line/get_next_line_bonus.h 	new file:   Get_Next_Line/get_next_line_utils.c 	new file:   Get_Next_Line/get_next_line_utils_bonus.c 	new file:   Instructions.pdf 	modified:   README.md

---
+++ Get_Next_Line/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhaksal <m.haksal@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/28 09:35:14 by mhaksal           #+#    #+#             */
+/*   Updated: 2022/02/28 09:37:45 by mhaksal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+int		ft_strlen(const char *s);
+char	*ft_strdup(const char *str);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strjoin(char *left_str, char *buff);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+int		f_strchck(char *s, char c);
+
+#endif
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Get_Next_Line
 A function that is used to read line by line from the given file
+
+Allowed functions:
+	-read,
+	-malloc,
+	-free
+
+Description:
+	Write a function that returns a line read from a file descriptor
+
+Requirements:
+	-Repeated calls should let you read one line at a time,
+	-The function should return the line that was read,
+	-The function should be able to read from standard input,
+	-Unless the eof is reached the terminating character should be '\n',
+	-Helper functions should be put in get_next_line_utils.c,
+	-Buffer size should be defined by the user,
+	-Global variables,
+	-Can't have any memory leaks for obvious reasons,
+	-Has to compile with the following flags: -Wall -Werror -Wextra
+
+Goal of the project:
+	-Teach static variables,
+	-Getting accustomed with reading and opening files using C functions
